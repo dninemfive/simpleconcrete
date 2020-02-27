54d8ccf4465c1039963cd255ada0b67b5aea56b5
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-02-27 09:51:47 -0800

    work on 1.1 update, gonna hardcode the IngredientValueWorker

---
+++ Source/D9Concrete/IngredientValueGetter_Concrete.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace D9Concrete
+{
+    public class IngredientValueGetter_Concrete : IngredientValueGetter
+    {
+        public override float ValuePerUnitOf(ThingDef t)
+        {
+            if (!t.IsStuff || !IsStoneyStuff(t)) return 0f;
+            if (t.HasModExtension<RecipeMultiplier>()) return t.GetModExtension<RecipeMultiplier>().multiplier;
+            return 1f;
+        }
+        public override string BillRequirementsDescription(RecipeDef r, IngredientCount ing)
+        {
+            return "BillRequires".Translate(ing.GetBaseCount(), ing.filter.Summary);
+        }
+        public static bool IsStoneyStuff(ThingDef t)
+        {
+            foreach (StuffCategoryDef scd in t.stuffProps.categories) if (scd == StuffCategoryDefOf.Stony) return true;
+            return false;
+        }
+    }
+    public class RecipeMultiplier : DefModExtension
+    {
+        public float multiplier; 
+    }
+}
